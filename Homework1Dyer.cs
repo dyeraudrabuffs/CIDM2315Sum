083338164cd2eaacfe7fb6861c18e9c08cc9c20f
dyeraudrabuffs <135450293+dyeraudrabuffs@users.noreply.github.com>
2023-06-05 19:06:17 -0500

    Homework 1 Code File Audra Dyer

---
+++ Homework1Dyer.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //calculate the value of z
+        double X = 2.5;
+        double Y = 3.3;
+        double z = 4*X*X + 3*Y; //4X squared plus 3*Y
+    
+        Console.WriteLine(z);  
+    }
+}
